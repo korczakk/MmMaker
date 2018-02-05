9bc0a26c5b705a3b2d08772574b445fc4a4e61b2
kkorc <kamil.korczak@bgk.pl>
2018-02-05 14:45:29 +0100

    Firebase

---
+++ MmMaker/Service/FirebaseConnector.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Firebase.Database;
+using Firebase.Database.Query;
+using Firebase.Auth;
+
+namespace MmMaker.Service
+{
+    public class FirebaseConnector
+    {
+        FirebaseClient _client;
+
+        public FirebaseConnector()
+        {
+            string appKey = "shoppinglist-dba72";
+
+            FirebaseAuthProvider auth = new FirebaseAuthProvider(appKey);
+
+            _client = new FirebaseClient("", new FirebaseOptions() { });
+        }
+    }
+}
